e84ab23b51ae9ade372235f7a8000f3e247300af
dekvall <dkvldev@gmail.com>
2020-12-03 15:03:03 +0100

    particle without resampling

--- python/filter_one.py
+++ python/filter_one.py
@@ -11,38 +11,23 @@ x0 = np.array([0, 2, 1.5, 0])
 R = 0.15**2 * np.eye(2)
 z0 = multivariate_normal(x0[:2], R)
 
+# For the filter
+invR = np.linalg.inv(R)
+
 
 # Initial state estimate
 xh0 = np.array([*z0, 1, 0])
 P0 = block_diag(R, 2**2 * np.eye(2))
 nX = 100 # Number of particles
 X = multivariate_normal(xh0, P0, size=nX)
-w = 1/nX * np.ones((1, nX))
+w = 1/nX * np.ones((nX,))
 
-fig = plt.figure()
-plt.scatter(X[:,0], X[:,1], marker=".", c='r', label="Particle")
-plt.xlabel("X [m]")
-plt.ylabel("Y [m]")
-ax = plt.gca()
-ax.set_xlim(-1, 11)
-ax.set_ylim(0, 5)
-plt.scatter(z0[0], z0[1], c='y', label="Measurement")
-
-
-# Ground truth
-tk = 1
-xk, *_ = propagate_state(0, tk, x0)
-zk = multivariate_normal(xk[:2], R)
 
 # Get for each ball
-X = np.apply_along_axis(lambda x: propagate_state(0, tk, x)[0], axis=1, arr=X)
+#X = np.apply_along_axis(lambda x: propagate_state(0, tk, x)[0], axis=1, arr=X)
 
-plt.scatter(X[:,0], X[:,1], marker=".", c='b', label="Propagated Particle")
-
-plt.legend()
-plt.grid()
-plt.show()
-
+#plt.scatter(X[:,0], X[:,1], marker=".", c='b', label="Propagated Particle")
+#plt.show()
 
 def prop_f(t_start, t_end, state, u=0, proc_noise=None):
 	x, *_ = propagate_state(t_start, t_end, state)
@@ -51,8 +36,12 @@ def prop_f(t_start, t_end, state, u=0, proc_noise=None):
 def measurement_f(t, x, u=0):
 	return x[:2]
 
+def obs_error_p(t, dz):
+	# Gaussian error which will be normalized later
+	return np.exp(-.5 * dz.T @ invR  @ dz)
+
 # particle filter
-def filter_for_one(t_before, t_k, X_before, w_before, u_before, z_k, f, h):
+def filter_for_one(t_before, t_k, X_before, w_before, u_before, z_k, f, h, obs_error):
 	"""
 	*_before: values for * at k-1
 	t_k: time for current measurement
@@ -66,9 +55,62 @@ def filter_for_one(t_before, t_k, X_before, w_before, u_before, z_k, f, h):
 	w_k: particle weights for k
 	"""
 
-	# Maybe just use a normal for-loop
-	X_new = np.apply_along_axis(lambda r: f(t_before, t_k, r), axis=1, arr=X_before)
-	Z_new = np.apply_along_axis(lambda r: h(t_k, r), axis=1, arr=X_new)
-	w_new = np.apply_along_axis(lambda r: r[0] * 0.3, arr=np.concatenate((X_new, Z_new))) # Actually observation error
+	N, nx = X_before.shape
+	nz, = z_k.shape
 
+
+	X_new = np.zeros((N, nx))
+	Z_new = np.zeros((N, nz))
+	w_new = np.zeros((N,))
+
+	for i in range(N):
+		X_new[i, :] = f(t_before, t_k, X_before[i, :])
+		Z_new[i, :] = h(t_k, X_new[i, :])
+		w_new[i] = w_before[i] * obs_error(t_k, \
+										z_k - Z_new[i, :])
+	
+	w_new /= w_new.max()
+
+	x_hat = np.average(X_new, axis=0, weights=w_new)
+
+	# resample here if needed
+
+	return x_hat, X_new, w_new
+
+	# Maybe just use a normal for-loop, yes
+	# X_new = np.apply_along_axis(lambda r: f(t_before, t_k, r), axis=1, arr=X_before)
+	# Z_new = np.apply_along_axis(lambda r: h(t_k, r), axis=1, arr=X_new)
+	# w_new = np.apply_along_axis(lambda r: r[0] * 0.3, arr=np.concatenate((X_new, Z_new))) # Actually observation error
+
+
+# Ground truth
+tk = 1
+xk, *_ = propagate_state(0, tk, x0)
+zk = multivariate_normal(xk[:2], R)
+
+xk = x0
+zk = z0
+
+dt = .2
+# Loop for 10 secs
+for t in np.arange(0,10,dt):
+	plt.scatter(X[:,0], X[:,1], marker=".", c='r', label="Particle")
+	plt.xlabel("X [m]")
+	plt.ylabel("Y [m]")
+	ax = plt.gca()
+	ax.set_xlim(-1, 11)
+	ax.set_ylim(0, 5)
+	plt.scatter(zk[0], zk[1], c='y', label="Measurement")
+
+	x_hat, X, w = filter_for_one(t, t+dt, X, w, 0, zk, prop_f, measurement_f, obs_error_p)
+	
+	plt.scatter(x_hat[0], x_hat[1], c='m', label="Approximation")
+	plt.scatter(xk[0], xk[1], c='g', label="Ground truth")
+	plt.legend()
+	plt.grid()
+	plt.pause(dt)
+	plt.clf()
+	# Ground truth
+	xk, *_ = propagate_state(t, t+dt, xk)
+	zk = multivariate_normal(xk[:2], R)
 
