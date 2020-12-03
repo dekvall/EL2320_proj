767cc573473dfa9ec1640ab16887de5f120008da
dekvall <dkvldev@gmail.com>
2020-12-03 01:50:18 +0100

    add propagation

--- python/propagate_state.py
+++ python/propagate_state.py
@@ -3,7 +3,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-points = []
 def propagate_point(t_start: float, t_end: float, state: list, dt: float =.05) -> list:
 	"""
 	in:
@@ -13,28 +12,30 @@ def propagate_point(t_start: float, t_end: float, state: list, dt: float =.05) -
 
 	return
 	propagated state at t_end
+	state- and time-trajectory
 	"""
 	G = 9.82
 	BOUNCE_COEFF = .84 # Tennis balls
 
 	#trajectory start
-	xt = state
-	tt = t_start
+	state_traj = [state]
+	time_traj = [t_start]
 
 	t = t_start
 
-	while t < t_end - 1e-16:
+	while t < t_end:
 		dt = min(dt, t_end - t)
 		t += dt
 
 		x, y, vx, vy = state
 
-		root = vy**2 + 2 * G * y
 
-		if root < 0:
+		tree = vy**2 + 2 * G * y
+		
+		if tree < 0:
 			dT = 0
 		else:
-			dT = (vy + root**.5) / G
+			dT = (vy + tree**.5) / G
 
 		# Never go back in time
 		dT = max(0, dT)
@@ -42,6 +43,11 @@ def propagate_point(t_start: float, t_end: float, state: list, dt: float =.05) -
 		if dT < dt:
 			# Bounce
 			vy = -BOUNCE_COEFF * (vy - G * dT)
+
+			# Save bounce state
+			state_traj.append([x + vx * dT, 0, vx, vy])
+			time_traj.append(t - dt + dT)
+
 			dT = dt - dT # Bounce time
 			y = 0 + vy * dT - .5 * G * dT**2
 			vy = vy - G * dT # update velocity for remainder f step
@@ -53,21 +59,16 @@ def propagate_point(t_start: float, t_end: float, state: list, dt: float =.05) -
 		x = x + vx * dt
 		vx = vx
 
-		state = np.array([x, y, vx, vy])
-		points.append(state)
-	return state
-
-
+		state = [x, y, vx, vy]
+		state_traj.append(state)
+		time_traj.append(t)
+	return np.array(state), np.array(state_traj), np.array(time_traj)
 
 
 if __name__ == "__main__":
-	points = []
-	x = np.array([0,2,.4, 0])
-	xn = propagate_point(0, 2, x)
+	x = np.array([0, 1, .4, 0])
+	x, xt, tt = propagate_point(0, 6, x)
 
-
-	rp = np.array(points)
-
-	plt.plot(rp[:,0], rp[:,1])
+	plt.plot(xt[:,0], xt[:,1])
+	plt.axhline(y=0, color='r', linestyle='-')
 	plt.show()
-
